474a9c9196edbb14c7d77e18f01bc187472f6520
David Kristek <djkristek@gmail.com>
2022-04-10 23:06:57 -0500

    added new tables in images

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -37,3 +37,11 @@ ON e.emp_no = ti.emp_no
 WHERE de.to_date = '9999-01-01' AND
 e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
 ORDER BY e.emp_no ASC;
+
+--Extra Tables
+--Mentorship Titles
+SELECT title, COUNT(emp_no)
+INTO mentorship_titles
+FROM mentorship_eligibilty
+Group BY title
+Order BY Count(emp_no) DESC;
